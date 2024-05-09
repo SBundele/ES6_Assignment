780fdcce962ff56b2071e30e35384cbe70fa5333
Sourabh Bundele <sbundele0318@gmail.com>
2024-05-09 12:36:45 +0530

    complete assignment 3

---
+++ Assignment_three/task_one.js
@@ -0,0 +1,23 @@
+// Example Invocation:
+
+const employees = [
+  { name: "John Doe", age: 30, department: "HR", salary: 50000 },
+  { name: "Jane Smith", age: 28, department: "Finance", salary: 60000 },
+  { name: "Alex Johnson", age: 35, department: "IT", salary: 70000 },
+];
+
+function highestPaid(employees) {
+  let mx_salary = -Infinity;
+  let highestSalaryEmployee = null;
+  for (let item of employees) {
+    let { salary } = item;
+    if (salary > mx_salary) {
+      mx_salary = salary;
+      highestSalaryEmployee = item;
+    }
+  }
+  return highestSalaryEmployee;
+}
+
+console.log(highestPaid(employees));
+// Output: { name: 'Alex Johnson', age: 35, department: 'IT', salary: 70000 }
---
+++ Assignment_three/task_two.js
@@ -0,0 +1,15 @@
+// Example Invocation:
+
+const employees = [
+  { name: "John Doe", age: 30, department: "HR", salary: 50000 },
+  { name: "Jane Smith", age: 28, department: "Finance", salary: 60000 },
+  { name: "Alex Johnson", age: 35, department: "IT", salary: 70000 },
+];
+
+function destructuringToSwap(employees){
+    let [employees1, employees2, employees3] = employees
+    return [employees3, employees2, employees1];
+}
+
+console.log(destructuringToSwap(employees)); 
+// Output: [ { name: 'Alex Johnson', age: 35, department: 'IT', salary: 70000 }, { name: 'Jane Smith', age: 28, department: 'Finance', salary: 60000 }, { name: 'John Doe', age: 30, department: 'HR', salary: 50000 } ]
